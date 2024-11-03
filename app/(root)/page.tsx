67a62abd5d32e2a8ddbbd80bc249487611156b71
subikstha <subsatch@gmail.com>
2024-11-03 07:39:16 +0545

    Tailwind css config and theme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-white">Welcome to the world of NextJS</h1>
+  <h1 className="h1-bold text-white">Welcome to the world of NextJS</h1>
 )
 
 export default Home
