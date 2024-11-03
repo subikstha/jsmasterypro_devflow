9a38614257996d5b6681d1889e0330a81730c455
subikstha <subsatch@gmail.com>
2024-11-03 08:02:22 +0545

    font setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,8 @@
 const Home = () => (
-  <h1 className="h1-bold text-white">Welcome to the world of NextJS</h1>
+  <>
+  <h1 className="h1-bold">Welcome to the world of NextJS</h1>
+  <h1 className="h1-bold font-space-grotesk">Welcome to the world of NextJS</h1>
+  </>
 )
 
 export default Home
