c1d4af742117690b7f58fd1538d55c0376698bab
subikstha <subsatch@gmail.com>
2024-11-05 22:04:19 +0545

    setup light and dark themes and shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
+
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/Inter.ttf",
@@ -29,12 +32,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        <h1 className="text-3xl">ROOT</h1>
+        <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem disableTransitionOnChange>
+        <Navbar />
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
