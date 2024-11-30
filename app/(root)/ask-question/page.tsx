798e6890e4fcb446bb5856690e0e3549b276e533
subikstha <subsatch@gmail.com>
2024-11-30 20:35:54 +0545

    Implement the UI of the question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import QuestionForm from '@/components/forms/QuestionForm';
 
 const AskAQuestion = () => {
   return (
-    <div>AskAQuestion</div>
-  )
-}
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
+};
 
-export default AskAQuestion
+export default AskAQuestion;
