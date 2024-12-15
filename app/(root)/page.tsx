d73066d6005a741e5ff1004ac4e8d1877f922e1f
subikstha <subsatch@gmail.com>
2024-12-15 19:26:45 +0545

    Implement Logging

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ import HomeFilter from '@/components/filters/HomeFilter';
 import LocalSearch from '@/components/search/LocalSearch';
 import { Button } from '@/components/ui/button';
 import ROUTES from '@/constants/routes';
+import handleError from '@/lib/handlers/error';
+import { NotFoundError, ValidationError } from '@/lib/http-errors';
 
 const questions = [
   {
@@ -47,11 +49,24 @@ const questions = [
   },
 ];
 
+const test = async () => {
+  try {
+    throw new ValidationError({
+      title: ['Required'],
+      tags: ['"Javascript" is not a valid tag'],
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const result = await test();
+  console.log(result);
   const { query = '', filter = '' } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from 'mongoose';
+import logger from './logger';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
@@ -26,6 +27,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info('Using existing mongoose connection');
     return cached.conn;
   }
 
@@ -35,11 +37,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: 'devflow',
       })
       .then((result) => {
-        console.log('Connected to MongoDB');
+        logger.info('Connected to MongoDB');
         return result;
       })
       .catch((error) => {
-        console.error('Error connecting to MongoDB', error);
+        logger.error('Error connecting to MongoDB', error);
         throw error;
       });
   }
