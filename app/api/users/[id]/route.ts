7afb0f55425f8a36cefd0364441ef985ccd5db78
subikstha <subsatch@gmail.com>
2025-01-07 22:08:32 +0545

    Implemented dynamic user by id

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,72 @@
+import { NextResponse } from 'next/server';
+
+import User from '@/database/user.model';
+import handleError from '@/lib/handlers/error';
+import { NotFoundError } from '@/lib/http-errors';
+import dbConnect from '@/lib/mongoose';
+import { UserSchema } from '@/lib/validations';
+
+// GET /api/users/[id]
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError('User');
+
+  try {
+    await dbConnect();
+    const user = await User.findById(id);
+
+    if (!user) throw new NotFoundError('User');
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, 'api') as APIErrorResponse;
+  }
+}
+
+// DELETE /api/users/[id]
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError('User');
+
+  try {
+    await dbConnect();
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) throw new NotFoundError('User');
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, 'api') as APIErrorResponse;
+  }
+}
+
+// PUT /api/users/[id]
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError('User');
+
+  try {
+    await dbConnect();
+    // Validating the data
+    const body = request.json();
+    const validatedData = UserSchema.partial().parse(body);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    }); // We put {new: true} so that we get the user after update is completed
+    if (!updatedUser) throw new NotFoundError('User');
+
+    return NextResponse.json({success: true, data: updatedUser}, {status: 200})
+  } catch (error) {
+    return handleError(error, 'api') as APIErrorResponse;
+  }
+}
