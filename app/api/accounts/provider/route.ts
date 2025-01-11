0ab569738d5e60d7b6145f745e9fe3e5b90ee689
subikstha <subsatch@gmail.com>
2025-01-11 16:30:51 +0545

    Implement Account by Provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+
+import Account from '@/database/account.model';
+import handleError from '@/lib/handlers/error';
+import { NotFoundError, ValidationError } from '@/lib/http-errors';
+import { AccountSchema } from '@/lib/validations';
+
+// Getting the user by email ID will be used in the auth to see if a user email exists and if so to get all the user's associated accounts
+export async function POST(request: Request) {
+  // This is a POST request because we want to extract the email from the body
+  const { providerAccountId } = await request.json();
+
+  try {
+    // First we try to validate the data
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+
+    if (!validatedData.success)
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError('Account');
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, 'api') as APIErrorResponse;
+  }
+}
