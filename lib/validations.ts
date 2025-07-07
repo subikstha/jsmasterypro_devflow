6f5880408330e9a2ff7f9dd06b43b629227134ac
subikstha <subsatch@gmail.com>
2025-07-07 07:31:46 +0545

    Enhance user answer with AI

--- lib/validations.ts
+++ lib/validations.ts
@@ -168,7 +168,8 @@ export const AIAnswerSchema = z.object({
     .string()
     .min(1, { message: 'Question is required' })
     .max(130, { message: 'Question cannot exceed 130 characters' }),
-  content: z.string().min(100, {
+  content: z.string().min(10, {
     message: 'Answer has to be at least 100 characters in length',
   }),
+  userAnswer: z.string().optional(),
 });
