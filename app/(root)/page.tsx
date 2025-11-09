2dbd463efea51e0e80a927e30877ef13739db103
subikstha <subsatch@gmail.com>
2025-11-09 18:55:25 +0545

    Get user stats action created, created a utility function for assigning badges

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,6 +12,7 @@ import { HomePageFilters } from '@/constants/filters';
 import ROUTES from '@/constants/routes';
 import { EMPTY_QUESTION } from '@/constants/states';
 import { getQuestions } from '@/lib/actions/question.action';
+import { getUserStats } from '@/lib/actions/user.action';
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -19,6 +20,9 @@ interface SearchParams {
 
 const Home = async ({ searchParams }: SearchParams) => {
   const session = await auth();
+  await getUserStats({
+    userId: session?.user?.id ?? '686e84a10b7a3947c62dabdb',
+  });
 
   const { page, pageSize, query, filter } = await searchParams;
 
@@ -28,8 +32,6 @@ const Home = async ({ searchParams }: SearchParams) => {
     query: query || '',
     filter: filter || '',
   });
-
-  console.log('Questions are', data, success, error);
 
   const { questions, isNext } = data || {};
   console.log('Session is', session);
--- constants/filters.ts
+++ constants/filters.ts
@@ -31,3 +31,31 @@ export const UserFilters = [
   { name: 'Oldest', value: 'oldest' },
   { name: 'Popular', value: 'popular' },
 ];
+
+export const BADGE_CRITERIA = {
+  QUESTION_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_COUNT: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  QUESTION_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  ANSWER_UPVOTES: {
+    BRONZE: 10,
+    SILVER: 50,
+    GOLD: 100,
+  },
+  TOTAL_VIEWS: {
+    BRONZE: 1000,
+    SILVER: 10000,
+    GOLD: 100000,
+  },
+};
